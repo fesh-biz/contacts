956518eed86645175f68cc0e630a417d2454ef37
fshelishkevich <feshbiz@gmail.com>
2024-11-25 01:20:29 +0200

    Contact: model, migration, controller, seeder, factory

---
+++ database/seeders/ContactSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Contact;
+use Illuminate\Database\Seeder;
+
+class ContactSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $contacts = Contact::factory(10)->make();
+
+        Contact::insert($contacts->toArray());
+    }
+}
